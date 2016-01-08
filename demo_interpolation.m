dac8e3a6f42ff71e8a6a9ee098afa2ba5c4e0eda
Jakub Kuszneruk <jakub.kuszneruk@adpilot.pl>
2016-01-08 11:33:03 +0100

    initial plot and test1
    bad householder implementation

--- demo_interpolation.m
+++ demo_interpolation.m
@@ -1,6 +1,52 @@
 source("lagrange_interpolation.m")
 
-x = [1; 2; 3];
-y = 2 * x;
+function x = chebyshevNodes(a, b, r)
+  x = zeros(1, r);
+  for j = 1:r
+    x(j) = (b - a)/2 * cos((2*j +1)/(2*r + 2) * pi) + (a + b)/2;
+  endfor
+endfunction
 
-wspNewton(x, y)
+function test1(f, range, space)
+  bigX = linspace(range(1), range(2), 256);
+  hold all;
+  plot(bigX, f(bigX)); # plot function
+  for n = [4, 64]
+    x = space(range(1), range(2), n);
+    y = f(x);
+    
+    c = wspNewton(x, y);
+    w = Horner(c, [], bigX);
+    plot(bigX, w);
+    #plot(x, y);
+  endfor
+endfunction
+
+f1 = @(x) cos(3*x);
+range1 = [-2 * pi; pi];
+f2 = @(x) abs(x);
+range2 = [-10; 10];
+
+# PLOT:
+#
+# cos(3*x) LINSPACE | cos(3*x) CHEBYSHEV
+# -------------------------------------
+# abs(x) LINSPACE   | abs(x) CHEBYSHEV
+#
+clf;
+
+subplot(2, 2, 1);
+ylim([-10, 10]);
+test1(f1, range1, @linspace);
+
+subplot(2, 2, 3);
+ylim([-10, 10]);
+test1(f2, range2, @linspace);
+
+subplot(2, 2, 2);
+ylim([-10, 10]);
+test1(f1, range1, @chebyshevNodes);
+
+subplot(2, 2, 4);
+ylim([-10, 10]);
+test1(f2, range2, @chebyshevNodes);
--- lagrange_interpolation.m
+++ lagrange_interpolation.m
@@ -1,10 +1,7 @@
 function c = wspNewton(x, y)
-  n = size(x);
-  b = x;
-  for j = 1:n
-    b(j) = y(j);
-  endfor
-  for j = 1:n
+  n = length(x);
+  b = y;
+  for j = 1:(n-1)
     for k = fliplr(j+1:n)
       b(k) = (b(k) - b(k-1)) / (x(k) - x(k-j));
     endfor
@@ -13,8 +10,10 @@ function c = wspNewton(x, y)
 endfunction
 
 function w = Horner(c, x, z)
-  v_n = a_n
-  for j = 1:(n-1)
-    v_j = v_{j+1} * x + a_j
+  n = length(c); # degree
+  m = length(z); # number of points to calculate
+  w = ones(1, m); # just to initialize vector of length n
+  for j = 1:n
+    w = w .* (z .- c(j));
   endfor
 endfunction
